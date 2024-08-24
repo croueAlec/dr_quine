5a8e55d26534cf6f5c4601c7ce9e5227a3deb11a
croueAlec <alec.croue@gmail.com>
2024-08-24 05:58:05 +0200

    ex01 done, fix in case file cant open

--- C/ex01/Grace.c
+++ C/ex01/Grace.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 #define destination "Grace_kid.c"
-#define code "#include <stdio.h>%1$c#include <fcntl.h>%1$c%1$c#define destination %2$c%3$s%2$c%1$c#define code %2$c%4$s%2$c%1$c#define ft int main(void) {FILE *file = fopen(destination, %2$cw%2$c); fprintf(file, code, 10, 34, destination, code); fclose(file); }%1$c%1$cft;%1$c/* %1$c	This runs the quine.%1$c */%1$c"
-#define ft int main(void) {FILE *file = fopen(destination, "w"); fprintf(file, code, 10, 34, destination, code); fclose(file); }
+#define code "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c%1$c#define destination %2$c%3$s%2$c%1$c#define code %2$c%4$s%2$c%1$c#define ft int main(void) {FILE *file = fopen(destination, %2$cw%2$c); if (!file) exit(1); fprintf(file, code, 10, 34, destination, code); fclose(file); }%1$c%1$cft;%1$c/* %1$c	This runs the quine.%1$c */%1$c"
+#define ft int main(void) {FILE *file = fopen(destination, "w"); if (!file) exit(1); fprintf(file, code, 10, 34, destination, code); fclose(file); }
 
 ft;
 /* 
